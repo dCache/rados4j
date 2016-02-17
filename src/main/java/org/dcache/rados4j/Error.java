56cd846caaf00f0374a9fbd04f08474491c2b5b7
Tigran Mkrtchyan <tigran.mkrtchyan@desy.de>
2016-02-17 13:23:16 +0100

    print error names, if possible

--- src/main/java/org/dcache/rados4j/Error.java
+++ src/main/java/org/dcache/rados4j/Error.java
@@ -1,5 +1,7 @@
 package org.dcache.rados4j;
 
+import jnr.constants.platform.Errno;
+
 public class Error {
 
     private Error() {}
@@ -7,7 +9,13 @@ public class Error {
     public static void checkError(jnr.ffi.Runtime runtime, int rc, String msg) throws RadosException {
         if (rc < 0) {
             int errno = runtime.getLastError();
-            throw new RadosException(msg + ": " + rc + " errno " + errno);
+            Errno e = Errno.valueOf(-rc);
+            Errno se = Errno.valueOf(errno);
+
+            throw new RadosException(
+                    String.format("%s : %s(%d), sys: %s(%d)",
+                            msg, e.name(), e.intValue(),
+                            se.name(), se.intValue()));
         }
     }
 }
